8f6e0ee4a1850047f7e438c7287fd0cd9f2a5632
Ralph Cajipe <ralphcajipe@gmail.com>
2022-02-07 00:50:16 +0800

    Complete 8 problems

--- machine_problems/mp2/1_horoscope.py
+++ machine_problems/mp2/1_horoscope.py
@@ -1,46 +1,41 @@
-def zodiac_sign(month, day):
-    """Check month and date within valid range of astrological chart."""
+month = int(input("Please enter your month of birth as a number:"))
+day = int(input("Please enter your day of birth as a number:"))
+astro_sign = None
 
-    if month == 1:
-        astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
+if month == 1:
+    astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
 
-    elif month == 2:
-        astro_sign = 'Aquarius' if (day < 19) else 'pisces'
+elif month == 2:
+    astro_sign = 'Aquarius' if (day < 19) else 'pisces'
 
-    elif month == 3:
-        astro_sign = 'Pisces' if (day < 21) else 'aries'
+elif month == 3:
+    astro_sign = 'Pisces' if (day < 21) else 'aries'
 
-    elif month == 4:
-        astro_sign = 'Aries' if (day < 20) else 'taurus'
+elif month == 4:
+    astro_sign = 'Aries' if (day < 20) else 'taurus'
 
-    elif month == 5:
-        astro_sign = 'Taurus' if (day < 21) else 'gemini'
+elif month == 5:
+    astro_sign = 'Taurus' if (day < 21) else 'gemini'
 
-    elif month == 6:
-        astro_sign = 'Gemini' if (day < 21) else 'cancer'
+elif month == 6:
+    astro_sign = 'Gemini' if (day < 21) else 'cancer'
 
-    elif month == 7:
-        astro_sign = 'Cancer' if (day < 23) else 'leo'
+elif month == 7:
+    astro_sign = 'Cancer' if (day < 23) else 'leo'
 
-    elif month == 8:
-        astro_sign = 'Leo' if (day < 23) else 'virgo'
+elif month == 8:
+    astro_sign = 'Leo' if (day < 23) else 'virgo'
 
-    elif month == 9:
-        astro_sign = 'Virgo' if (day < 23) else 'libra'
+elif month == 9:
+    astro_sign = 'Virgo' if (day < 23) else 'libra'
 
-    elif month == 10:
-        astro_sign = 'Libra' if (day < 23) else 'scorpio'
+elif month == 10:
+    astro_sign = 'Libra' if (day < 23) else 'scorpio'
 
-    elif month == 11:
-        astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
+elif month == 11:
+    astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
 
-    elif month == 12:
-        astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
+elif month == 12:
+    astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
 
-    print(f"You are a {astro_sign.title()}")
-
-
-user_month = int(input("Please enter your month of birth as a number:"))
-user_day = int(input("Please enter your day of birth as a number:"))
-
-zodiac_sign(user_month, user_day)
+print(f"You are a {astro_sign.title()}")
--- machine_problems/mp2/3_letter_grade.py
+++ machine_problems/mp2/3_letter_grade.py
@@ -1,30 +1,44 @@
 letter_grade = input("Enter grade:")
+grade_pts = 0
 
 if letter_grade == "A+":
     grade_pts = 4.0
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "A":
     grade_pts = 4.0
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "A-":
     grade_pts = 3.7
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "B+":
     grade_pts = 3.3
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "B":
     grade_pts = 3.0
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "B-":
     grade_pts = 2.7
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "C+":
     grade_pts = 2.3
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "C":
     grade_pts = 2.0
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "C-":
     grade_pts = 1.7
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "D+":
     grade_pts = 1.3
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "D":
     grade_pts = 1.0
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 elif letter_grade == "F":
     grade_pts = 0
+    print(f"For this grade, you will receive a grade points of {grade_pts}")
 else:
     print("Invalid grade entered.")
 
-print(f"For this grade, you will receive a grade points of {grade_pts}")
+
+
