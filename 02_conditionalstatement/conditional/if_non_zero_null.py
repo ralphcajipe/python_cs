a6a207727a3d9cf618b89f707d07ab9c3b73d284
Ralph Cajipe <ralphcajipe@gmail.com>
2022-02-13 17:03:28 +0800

    Add more examples from lecture

---
+++ 02_conditionalstatement/conditional/if_non_zero_null.py
@@ -0,0 +1,16 @@
+# if a variable has non-zero and non-null values,
+# it is True, we print that there is true expression.
+
+var1 = 100
+if var1:
+    print("1 - Got a true expression value")
+    print(var1)
+
+print()
+
+var2 = 0
+if var2:
+    print("2 - Got a true expression value")
+    print(var2)
+
+print("Exit")
---
+++ 02_conditionalstatement/conditional/if_positive.py
@@ -0,0 +1,11 @@
+# if the number is positive, we print an appropriate message.
+
+number = 3
+if number > 0:
+    print(number, "is a positive number.")
+print("This is always printed btw.")
+
+number = -1
+if number > 0:
+    print(number, "is a positive number.")
+print("This is also always printed.")
