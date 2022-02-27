8103f82e2f46a0f5a3485fc1f04628ecc6174431
Ralph Cajipe <ralphcajipe@gmail.com>
2022-02-27 19:45:07 +0800

    Traversing in a 2D list

---
+++ 05_data_types/list/2d_list.py
@@ -0,0 +1,9 @@
+colors = [
+           ["orange", "purple", "green"],
+           ["blue", "white", "brown"],
+           ["black", "red", "yellow"],
+         ]
+
+for row in colors:
+    for color in row:
+        print(color)
